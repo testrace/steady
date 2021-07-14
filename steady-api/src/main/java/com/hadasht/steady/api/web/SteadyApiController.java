4260c192546b660bc89d82b1e2efbb7f90380676
yongju <odetsuo@gmail.com>
2021-07-14 19:24:10 +0900

    [API] ADD : api list and finish feat

--- steady-api/src/main/java/com/hadasht/steady/api/web/SteadyApiController.java
+++ steady-api/src/main/java/com/hadasht/steady/api/web/SteadyApiController.java
@@ -1,13 +1,37 @@
 package com.hadasht.steady.api.web;
 
+import com.hadasht.steady.api.dto.SteadyDto;
+import com.hadasht.steady.core.steady.domain.Steady;
+import com.hadasht.steady.core.steady.service.SteadyService;
+import com.hadasht.steady.utils.ApiResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
+@RequiredArgsConstructor
 public class SteadyApiController {
 
+	private final SteadyService steadyService;
+
 	@GetMapping("/api/list")
-	public String list() {
-		return "list";
+	public ApiResponse list() {
+		List<Steady> listForToday = steadyService.getListForToday();
+		List<SteadyDto> steadyDtos = listForToday.stream().map(SteadyDto::from).collect(Collectors.toList());
+		return ApiResponse.success(steadyDtos);
 	}
+
+	@PostMapping("/api/finished/{id}")
+	public ApiResponse finished(@PathVariable Long id) {
+		Long finishedId = steadyService.finished(id);
+
+		return ApiResponse.success(finishedId);
+	}
+
+
 }
