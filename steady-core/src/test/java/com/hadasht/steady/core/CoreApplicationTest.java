b2c8541271c35b95a9e9e65810f61aaaeae740cb
yongju <odetsuo@gmail.com>
2021-07-14 13:01:42 +0900

    [CORE] ADD : 테스트용 SpringBootApplication

---
+++ steady-core/src/test/java/com/hadasht/steady/core/CoreApplicationTest.java
@@ -0,0 +1,11 @@
+package com.hadasht.steady.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplicationTest {
+
+	public void contextLoad() {
+	}
+
+}
