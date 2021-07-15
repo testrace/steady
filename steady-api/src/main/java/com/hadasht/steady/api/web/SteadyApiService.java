5504277f9894a0507d8b96ac02ed8e48984e9013
yongju <odetsuo@gmail.com>
2021-07-15 16:08:17 +0900

    [API] ADD : api용 서비스 분리

---
+++ steady-api/src/main/java/com/hadasht/steady/api/web/SteadyApiService.java
@@ -0,0 +1,29 @@
+package com.hadasht.steady.api.web;
+
+import com.hadasht.steady.api.dto.SteadyDto;
+import com.hadasht.steady.core.steady.domain.Steady;
+import com.hadasht.steady.core.steady.service.SteadyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SteadyApiService {
+
+	private final SteadyService steadyService;
+
+	public List<SteadyDto> getSteadyToday() {
+		List<Steady> listForToday = steadyService.getListForToday();
+		return listForToday.stream()
+		                   .map(SteadyDto::from)
+		                   .collect(Collectors.toList());
+	}
+
+	public SteadyDto finished(Long id) {
+		return SteadyDto.from(steadyService.finished(id));
+	}
+
+}
