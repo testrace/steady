36987bee8a41b933e5cbbeed670b3d7afb7a4aed
yongju <odetsuo@gmail.com>
2021-07-15 16:09:21 +0900

    [API] ADD : steady day 변수

--- steady-api/src/main/java/com/hadasht/steady/api/dto/SteadyDto.java
+++ steady-api/src/main/java/com/hadasht/steady/api/dto/SteadyDto.java
@@ -4,6 +4,8 @@ import com.hadasht.steady.core.steady.domain.Steady;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 @Builder
 public class SteadyDto {
@@ -11,11 +13,13 @@ public class SteadyDto {
 	private Long steadyId;
 	private String steadyName;
 	private boolean finished;
+	private LocalDate steadyDay;
 
 	public static SteadyDto from(Steady steady) {
 		return SteadyDto.builder()
 		                .steadyId(steady.getId())
 						.steadyName(steady.getTemplate().getSteadyName())
+						.steadyDay(steady.getSteadyDay())
 						.finished(steady.isFinished())
 						.build();
 	}
