c42df1c08a338b60f4e2b204754e94fffc759b03
yongju <odetsuo@gmail.com>
2021-07-16 15:58:37 +0900

    [CLIENT] ADD : feature list and finished toggle

--- steady-client-react/src/components/SteadyList.js
+++ steady-client-react/src/components/SteadyList.js
@@ -8,7 +8,8 @@ import {
     ListItemText
 } from "@material-ui/core";
 import CommentIcon from "@material-ui/icons/Comment";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import Api from "../utils/Api";
 
 
 
@@ -33,11 +34,29 @@ export default function SteadyList(classes) {
         ]
     });
 
+    useEffect(() => {
+        Api.getList()
+            .then((response) => {
+                console.log(response);
+                setCheckList(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+
     const handleToggle = (value) => () => {
         setCheckList(
             {
-                data : checkList.data.map(data =>
-                    (data.id === value) ? {...data, checked : !data.checked} : data
+                data : checkList.data.map(data => {
+                        if (data.steadyId === value) {
+                            Api.finished(value);
+                            return {...data, finished: !data.finished}
+                        } else {
+                            return data;
+                        }
+                    }
                 )
             }
         )
@@ -46,20 +65,20 @@ export default function SteadyList(classes) {
     return (
         <List className={classes.root}>
             {checkList.data.map((data) => {
-                const labelId = `checkbox-list-label-${data.id}`;
+                const labelId = `checkbox-list-label-${data.steadyId}`;
 
                 return (
-                    <ListItem key={data.id} role={undefined} dense button onClick={handleToggle(data.id)}>
+                    <ListItem key={data.steadyId} role={undefined} dense button onClick={handleToggle(data.steadyId)}>
                         <ListItemIcon>
                             <Checkbox
                                 edge="start"
-                                checked={data.checked}
+                                checked={data.finished}
                                 tabIndex={-1}
                                 disableRipple
                                 inputProps={{ 'aria-labelledby': labelId }}
                             />
                         </ListItemIcon>
-                        <ListItemText id={labelId} primary={`Line item ${data.name}`} secondary={`${data.startTime}~${data.endTime}`}/>
+                        <ListItemText id={labelId} primary={`${data.steadyName}`} secondary={`${data.startTime}~${data.endTime}`}/>
                         <ListItemSecondaryAction>
                             <IconButton edge="end" aria-label="comments">
                                 <CommentIcon />
