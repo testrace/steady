19b6adb3280f51d82bb27547d53b70b52cd46b53
yongju <odetsuo@gmail.com>
2021-07-16 12:55:21 +0900

    [API] FIX : start and end time in dto

--- steady-api/src/main/java/com/hadasht/steady/api/dto/SteadyDto.java
+++ steady-api/src/main/java/com/hadasht/steady/api/dto/SteadyDto.java
@@ -1,10 +1,12 @@
 package com.hadasht.steady.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.hadasht.steady.core.steady.domain.Steady;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Data
 @Builder
@@ -13,12 +15,18 @@ public class SteadyDto {
 	private Long steadyId;
 	private String steadyName;
 	private boolean finished;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+	private LocalTime startTime;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+	private LocalTime endTime;
 	private LocalDate steadyDay;
 
 	public static SteadyDto from(Steady steady) {
 		return SteadyDto.builder()
 		                .steadyId(steady.getId())
 						.steadyName(steady.getTemplate().getSteadyName())
+		                .startTime(steady.getTemplate().getStartTime())
+		                .endTime(steady.getTemplate().getEndTime())
 						.steadyDay(steady.getSteadyDay())
 						.finished(steady.isFinished())
 						.build();
